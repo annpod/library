65ab43e3080952363de8000a07ed0661166f5c39
Anna <APodoynitsyna@smartspaceplc.com>
2021-03-11 20:34:27 +0200

    multi entry points

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ["./lib"],
+  roots: ["./src"],
   setupFilesAfterEnv: ["./jest.setup.ts"],
   moduleFileExtensions: ["ts", "tsx", "js"],
   testPathIgnorePatterns: ["node_modules/"],
--- rollup.config.js
+++ rollup.config.js
@@ -7,27 +7,28 @@ import json from '@rollup/plugin-json';
 // import svg from 'rollup-plugin-svg';
 import image from '@rollup/plugin-image';
 // import url from "rollup-plugin-url"
-
+import multi from '@rollup/plugin-multi-entry';
 // const packageJson = require('./package.json');
 
 export default {
-  input: ['lib/index.tsx', "lib/components/collapse-item/index.ts"],
-  output: [
-    {
-      dir: "build",
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true
-    },
-    // {
-    //   file: packageJson.module,
-    //   format: 'esm',
-    //   exports: 'named',
-    //   sourcemap: true
-    // }
-  ],
+  input: ['src/index.tsx', "src/components/**/index.ts"],
+  // output: [
+  //   {
+  //     dir: "build",
+  //     format: 'cjs',
+  //     exports: 'named',
+  //     sourcemap: true
+  //   },
+  //   // {
+  //   //   file: "build/index.js",
+  //   //   format: 'esm',
+  //   //   exports: 'named',
+  //   //   sourcemap: true
+  //   // }
+  // ],
   preserveModules: true,
   plugins: [
+    multi(),
     image(),
     // svg(),    
     peerDepsExternal({
@@ -40,10 +41,10 @@ export default {
       rollupCommonJSResolveHack: true,
       clean: true,
       exclude: [
-        'lib/**/*.stories.tsx',
-        'lib/**/*.test.(tsx|ts)',
-        'lib/**/*.stories.tsx',
-        'lib/**/*.svg'
+        'src/**/*.stories.tsx',
+        'src/**/*.test.(tsx|ts)',
+        'src/**/*.stories.tsx',
+        'src/**/*.svg'
       ]
     }),
     commonjs(),
