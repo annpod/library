7def87a7dde5952718546b36ac89bdd5b6c5cb8d
Anna <APodoynitsyna@smartspaceplc.com>
2021-04-12 21:34:36 +0300

    test

---
+++ src/extended-select/extended-select.d.ts
@@ -0,0 +1,9 @@
+
+export declare interface IExtendedSelectOption {
+  keyValue: string;
+  value: any;
+  options: ISelectOption[];
+  placeholder: string;
+  isDisabled?: boolean;
+  optionValue?: string;
+}
