b5b9b3e6a7ecc88ea852b1a49a56b92ebec1ce0b
Anna <APodoynitsyna@smartspaceplc.com>
2021-04-13 21:16:39 +0300

    test

--- rollup.config.js
+++ rollup.config.js
@@ -42,9 +42,6 @@ export default [
         useTsconfigDeclarationDir: true,
         rollupCommonJSResolveHack: true,
         clean: true,
-        declaration: true,
-        declarationDir: 'lib/types/',
-        rootDir: 'src/',
         exclude: [
           'node_modules',
           'src/**/*.stories.tsx',
@@ -58,70 +55,70 @@ export default [
       json()
     ]
   },
-  {
-    input: [
-      'src/button/index.ts',
-      'src/collapse-item/index.ts',
-      'src/custom-search-input/index.ts',
-      'src/date-picker/index.ts',
-      'src/desks/index.ts',
-      'src/extended-select/index.ts',
-      'src/fetch-table/index.ts',
-      'src/form/index.ts',
-      'src/location-filter/index.tsx',
-      'src/modal/index.ts',
-      'src/modal-button/index.ts',
-      'src/icon/index.ts',
-      'src/pagination/index.ts',
-      'src/scrollbars/index.ts',
-      'src/params-panel/index.ts',
-      'src/search-input/index.ts',
-      'src/select/index.ts',
-      'src/time-period-input/index.ts',
-      'src/tab-button/index.ts',
-      'src/time-picker/index.ts',
-      'src/time-range-picker/index.ts',
-      'src/custom-search-input/index.ts',
-      'src/header/index.ts',
-      'src/hooks/index.ts',
-      'src/typings/index.ts',
-      'src/constants/strings.ts'
-    ],
-    output: [
-      {
-        dir: 'lib',
-        format: 'cjs',
-        sourcemap: true,
-        exports: 'named'
-      },
-      {
-        dir: 'lib',
-        format: 'esm',
-        exports: 'named',
-        sourcemap: true
-      }
-    ],
-    preserveModules: true,
-    plugins: [
-      image(),
-      peerDepsExternal({
-        includeDependencies: true
-      }),
-      resolve(),
-      postcss(),
-      typescript({
-        rollupCommonJSResolveHack: true,
-        useTsconfigDeclarationDir: true,
-        clean: true,
-        exclude: [
-          'node_modules',
-          'src/**/*.stories.tsx',
-          'src/**/*.test.(tsx|ts)',
-          'src/**/*.svg'
-        ]
-      }),
-      commonjs(),
-      json()
-    ]
-  }
+  // {
+  //   input: [
+  //     'src/button/index.ts',
+  //     'src/collapse-item/index.ts',
+  //     'src/custom-search-input/index.ts',
+  //     'src/date-picker/index.ts',
+  //     'src/desks/index.ts',
+  //     'src/extended-select/index.ts',
+  //     'src/fetch-table/index.ts',
+  //     'src/form/index.ts',
+  //     'src/location-filter/index.tsx',
+  //     'src/modal/index.ts',
+  //     'src/modal-button/index.ts',
+  //     'src/icon/index.ts',
+  //     'src/pagination/index.ts',
+  //     'src/scrollbars/index.ts',
+  //     'src/params-panel/index.ts',
+  //     'src/search-input/index.ts',
+  //     'src/select/index.ts',
+  //     'src/time-period-input/index.ts',
+  //     'src/tab-button/index.ts',
+  //     'src/time-picker/index.ts',
+  //     'src/time-range-picker/index.ts',
+  //     'src/custom-search-input/index.ts',
+  //     'src/header/index.ts',
+  //     'src/hooks/index.ts',
+  //     'src/typings/index.ts',
+  //     'src/constants/strings.ts'
+  //   ],
+  //   output: [
+  //     {
+  //       dir: 'lib',
+  //       format: 'cjs',
+  //       sourcemap: true,
+  //       exports: 'named'
+  //     },
+  //     {
+  //       dir: 'lib',
+  //       format: 'esm',
+  //       exports: 'named',
+  //       sourcemap: true
+  //     }
+  //   ],
+  //   preserveModules: true,
+  //   plugins: [
+  //     image(),
+  //     peerDepsExternal({
+  //       includeDependencies: true
+  //     }),
+  //     resolve(),
+  //     postcss(),
+  //     typescript({
+  //       rollupCommonJSResolveHack: true,
+  //       useTsconfigDeclarationDir: true,
+  //       clean: true,
+  //       exclude: [
+  //         'node_modules',
+  //         'src/**/*.stories.tsx',
+  //         'src/**/*.test.(tsx|ts)',
+  //         'src/**/*.svg'
+  //       ]
+  //     }),
+  //     commonjs(),
+  //     json()
+  //   ]
+  // }
 ];
