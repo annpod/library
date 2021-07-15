ec719c6facb14ef62f5d6a7cbbeaa0436f4090c6
Anna <APodoynitsyna@smartspaceplc.com>
2021-07-15 18:07:56 +0300

    fix styles

--- src/toast/toast.styled.ts
+++ src/toast/toast.styled.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { ToastContainer } from 'react-toastify';
 
 export const Toast = styled(ToastContainer)`
-  && {
     .Toastify__toast {
       border-width: 1px;
       border-style: solid;
@@ -12,45 +11,42 @@ export const Toast = styled(ToastContainer)`
       font-family: 'Gotham';
       word-break: break-word;
       min-height: 45px;
+    }      
+    .Toastify__toast--error {
+      ${(props) => `
+      color: ${props.theme.toast.error.color};
+      background: ${props.theme.toast.error.background};
+      border-color: ${props.theme.toast.error.borderColor};
+      `}
     }
-      
-      .Toastify__toast--error {
-        ${(props) => `
-        color: ${props.theme.toast.error.color};
-        background: ${props.theme.toast.error.background};
-        border-color: ${props.theme.toast.error.borderColor};
-        `}
-      }
-      .Toastify__toast--warn {
-        ${(props) => `
-        color: ${props.theme.toast.warn.color};
-        background: ${props.theme.toast.warn.background};
-        border-color: ${props.theme.toast.warn.borderColor};
-        `}
-      }
-      .Toastify__toast--info {
-        ${(props) => `
-        color: ${props.theme.toast.info.color};
-        background: ${props.theme.toast.info.background};
-        border-color: ${props.theme.toast.info.borderColor};
-        `}
-      }
-      .Toastify__toast--success {
-        ${(props) => `
-        color: ${props.theme.toast.success.color};
-        background: ${props.theme.toast.success.background};
-        border-color: ${props.theme.toast.success.borderColor};
-        `}
-      }
-      .Toastify__toast--default {
-        ${(props) => `
-        color: ${props.theme.toast.default.color};
-        background: ${props.theme.toast.default.background};
-        border-color: ${props.theme.toast.default.borderColor};
-        `}
-      }
-      
+    .Toastify__toast--warn {
+      ${(props) => `
+      color: ${props.theme.toast.warn.color};
+      background: ${props.theme.toast.warn.background};
+      border-color: ${props.theme.toast.warn.borderColor};
+      `}
     }
+    .Toastify__toast--info {
+      ${(props) => `
+      color: ${props.theme.toast.info.color};
+      background: ${props.theme.toast.info.background};
+      border-color: ${props.theme.toast.info.borderColor};
+      `}
+    }
+    .Toastify__toast--success {
+      ${(props) => `
+      color: ${props.theme.toast.success.color};
+      background: ${props.theme.toast.success.background};
+      border-color: ${props.theme.toast.success.borderColor};
+      `}
+    }
+    .Toastify__toast--default {
+      ${(props) => `
+      color: ${props.theme.toast.default.color};
+      background: ${props.theme.toast.default.background};
+      border-color: ${props.theme.toast.default.borderColor};
+      `}
+    }    
     .Toastify__toast-body {
       padding: 0;
     }
@@ -60,6 +56,5 @@ export const Toast = styled(ToastContainer)`
         color: #707070;
         display: block;
       }
-    }
-  }
+    }  
 `;
