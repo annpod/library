3fea700cfecc9552a08be9abc2f1f963d69653b1
annpod <ann.podoynityna@gmail.com>
2021-08-05 18:46:02 +0300

    test

--- src/breadcrumbs/breadcrumbs.tsx
+++ src/breadcrumbs/breadcrumbs.tsx
@@ -1,30 +1,31 @@
-import React from "react";
+import React from 'react';
 // import { NavLink } from "react-router-dom";
-import { IBreadcrumbRouteMatch  } from '../typings';
-
+import { IBreadcrumbRouteMatch } from '../typings';
 
 interface IBreadcrumbs {
   breadcrumbs: IBreadcrumbRouteMatch[];
   lastItem: string;
 }
 
-export const Breadcrumbs = ({ 
-  breadcrumbs, 
-  lastItem,
- }: IBreadcrumbs) => (
+export const Breadcrumbs = ({ breadcrumbs, lastItem }: IBreadcrumbs) => (
   <div>
-    {breadcrumbs.map((item: IBreadcrumbRouteMatch, index: number) => (
-      <span key={item.path}>
-        {!lastItem && index === breadcrumbs.length - 1 ? (
-          <div>{item.breadcrumb}</div>
-        ) : (
-          <>
-            <a href={item.match.url}>{item.breadcrumb}</a>
-            <span>{" > "}</span>
-          </>
-        )}
-      </span>
-    ))}
+    {breadcrumbs.map(
+      (item: IBreadcrumbRouteMatch, index: number) =>
+        item && (
+          <span key={`br-${index}`}>
+            {!lastItem && index === breadcrumbs.length - 1 ? (
+              <div>{item.breadcrumb}</div>
+            ) : (
+              <>
+                {/* <a href={item.match.url}>{item.breadcrumb}</a> */}
+                {item.match.url}
+                {item.breadcrumb}
+                <span>{' > '}</span>
+              </>
+            )}
+          </span>
+        )
+    )}
     {lastItem && <div>{lastItem}</div>}
   </div>
 );
