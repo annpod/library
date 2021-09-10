5b24f4fce46f8a48225d07506ceccb761acb4db5
Anna <APodoynitsyna@smartspaceplc.com>
2021-09-10 18:27:34 +0300

    new drawer

--- src/drawer/drawer.tsx
+++ src/drawer/drawer.tsx
@@ -1,82 +1,133 @@
 import React from 'react';
-import styled, { withTheme } from 'styled-components';
+import styled from 'styled-components';
 
-import { Drawer as DrawerAntd } from 'antd';
-import { IDrawer } from '../typings';
+// import { Drawer as DrawerAntd } from 'antd';
+// import { IDrawer } from '../typings';
 import 'antd/lib/drawer/style/css';
-import close from '../icon/icons/close.svg';
 import { ButtonStyled } from '../button';
+import { IconButton } from '../icon-button';
+import { Icon } from '../icon';
+import { CustomScrollbars } from '../scrollbars';
 
-const DrawerC = ({
-  visible,
-  children,
-  headerTitle,
-  onCancel,
-  onClose,
-  onOk,
-  okText,
-  cancelText,
-  okButtonProps,
-  cancelButtonProps,
-  afterVisibleChange,
-}: IDrawer) => (
-  <DrawerComponent
-    title={headerTitle}
-    closable={true}
-    onClose={onClose}
-    visible={visible}
-    width={'292px'}
-    afterVisibleChange={afterVisibleChange}
-    data-location='drawer'
-  >
-    <>
-    {children}
-    <Footer>
-      {okText && (
-        <ButtonStyled.Primary
-          data-location='okButton'
-          onClick={onOk}
-          disabled={okButtonProps && okButtonProps.disabled}
-        >
-          {okText}
-        </ButtonStyled.Primary>
-      )}
-      {cancelText && (
-        <ButtonStyled.Light
-          data-location='cancelButton'
-          onClick={onCancel}
-          disabled={cancelButtonProps && cancelButtonProps.disabled}
-        >
-          {cancelText}
-        </ButtonStyled.Light>
-      )}
-    </Footer>
-    </>
-  </DrawerComponent>
-);
+const Overlay = styled.div`
+  background: rgba(0, 0, 0, 0.5);
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 20;
+`;
 
-export const Drawer: any = withTheme(DrawerC as any);
-
-const DrawerComponent = styled(DrawerAntd)`
- .ant-drawer-content {
-    max-width: 292px;
-    margin: 0;
-    .ant-drawer-header {
-      background: #f7f7f7;
-      color: #434448;
-      font-size: 14px;
+const FlyoutContainer = styled.div<{ slideInAnimation?: boolean }>`
+  @keyframes slideIn {
+    0% {
+      transform: translateX(100%);
     }
-    .ant-drawer-close {
-      background: url(${close}) no-repeat center center;
-      .anticon {
-        display: none;
-      }
+    100% {
+      transform: translateX(0);
     }
   }
+
+  @keyframes slideOut {
+    0% {
+      transform: translateX(0%);
+    }
+    100% {
+      transform: translateX(100%);
+    }
+  }
+
+  display: grid;
+  width: 292px;
+  grid-template-rows: 50px 1fr 50px;
+  background: #ffffff;
+  position: fixed;
+  z-index: 40;
+  top: 0;
+  right: 0;
+  bottom:0;
+  height: 100vw;
+  ${(props) =>
+    props.slideInAnimation
+      ? `animation: 0.5s ease-in slideIn`
+      : `animation: 0.5s ease-out slideOut`}
 `;
+
+const Header = styled.div`
+  border-bottom: 1px solid ${(props) => props.theme.borderColorLight};
+  padding: 0px 20px;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  justify-content: space-between;
+  background: #f7f7f7;
+  height: 54px;
+`;
+
+const Form = styled.div`
+  padding: 20px 20px;
+`;
+
+// interface IDrawer {
+//   backRoute: string;
+//   children?: React.ReactNode;
+//   itemCountString: string | null;
+//   goBack: () => void;
+//   isMenuOpen?: boolean;
+// }
+
+export const Drawer = (props: any) => {
+  const { children, headerTitle, goBack, visible } = props;
+
+  return (
+    <>
+      <Overlay onClick={goBack} />
+      <FlyoutContainer slideInAnimation={visible} data-location='drawer'>
+        <Header>
+          <span>{headerTitle}</span>
+          <IconButton
+            className='close-button'
+            icon={<Icon size={25} type='close' />}
+            onClick={props.onCancel}
+          />
+        </Header>
+        <CustomScrollbars>
+          <Form>{children}</Form>
+          <Footer>
+            {props.okText && (
+              <ButtonStyled.Primary
+                data-location='okButton'
+                onClick={props.onOk}
+                disabled={props.okButtonProps && props.okButtonProps.disabled}
+              >
+                {props.okText}
+              </ButtonStyled.Primary>
+            )}
+            {props.cancelText && (
+              <ButtonStyled.Light
+                data-location='cancelButton'
+                onClick={props.onCancel}
+                disabled={
+                  props.cancelButtonProps && props.cancelButtonProps.disabled
+                }
+              >
+                {props.cancelText}
+              </ButtonStyled.Light>
+            )}
+          </Footer>
+        </CustomScrollbars>
+      </FlyoutContainer>
+    </>
+  );
+};
+
 const Footer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
   padding: 30px 0;
+  padding: 20px 20px;
 `;
