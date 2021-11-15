a8f85ee606f67609f8a71e785173f52136a67b9c
Anna <APodoynitsyna@smartspaceplc.com>
2021-11-15 15:45:39 +0200

    test font

--- src/theme-provider/global-styles.ts
+++ src/theme-provider/global-styles.ts
@@ -1,6 +1,19 @@
 import { createGlobalStyle } from 'styled-components';
+import GothamMediumSvg from '../fonts/book/gotham-book.svg';
+const GothamMediumEot = require('../fonts/book/gotham-book.eot');
+const GothamMediumWoff = require('../fonts/book/gotham-book.woff');
+const GothamMediumTtf = require('../fonts/book/gotham-book.ttf');
 
 export const GlobalStyles = createGlobalStyle`
+@font-face {
+  font-family: 'Gotham-Medium';
+  src: url(${GothamMediumEot}) format('eot');
+  src: url(${GothamMediumSvg}) format('svg'),
+  url(${GothamMediumTtf}) format('ttf'),
+  url(${GothamMediumWoff}) format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
   body {
     font-family: 'Gotham-Medium';
     margin: 0;
