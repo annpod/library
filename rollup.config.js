eb1556de5e8a95a1dc715609883b36c0a73bc067
Anna <APodoynitsyna@smartspaceplc.com>
2021-11-16 12:07:03 +0200

    test

--- rollup.config.js
+++ rollup.config.js
@@ -45,8 +45,6 @@ export default [
       }),
       resolve(),
       postcss({
-        extensions: ['.css'],
-        extract: true,
         plugins: [
           postcssFontGrabber({
             // postcss-font-grabber needs to know the CSS output
@@ -76,5 +74,68 @@ export default [
       }),          
     ]
   },
-
+  {
+    input: [
+      'src/button/index.ts',
+      'src/collapse-item/index.ts',
+      'src/custom-search-input/index.ts',
+      'src/date-picker/index.ts',
+      'src/extended-select/index.ts',
+      'src/fetch-table/index.ts',
+      'src/form/index.ts',
+      'src/location-filter/index.ts',
+      'src/modal/index.ts',
+      'src/modal-button/index.ts',
+      'src/icon/index.ts',
+      'src/pagination/index.ts',
+      'src/scrollbars/index.ts',
+      'src/search-input/index.ts',
+      'src/select/index.ts',
+      'src/time-period-input/index.ts',
+      'src/tab-button/index.ts',
+      'src/time-picker/index.ts',
+      'src/time-range-picker/index.ts',
+      'src/custom-search-input/index.ts',
+      'src/header/index.ts',
+      'src/hooks/index.ts',
+      'src/typings/index.ts',
+      'src/constants/strings.ts'
+    ],
+    output: [
+      {
+        dir: 'lib',
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named'
+      },
+      {
+        dir: 'lib',
+        format: 'esm',
+        exports: 'named',
+        sourcemap: true
+      }
+    ],
+    preserveModules: true,
+    plugins: [
+      image(),
+      peerDepsExternal({
+        includeDependencies: true
+      }),
+      resolve(),
+      postcss(),
+      typescript({
+        rollupCommonJSResolveHack: true,
+        useTsconfigDeclarationDir: true,
+        clean: true,
+        exclude: [
+          'node_modules',
+          'src/**/*.stories.tsx',
+          'src/**/*.test.(tsx|ts)',
+          'src/**/*.svg'
+        ]
+      }),
+      commonjs(),
+      json()
+    ]
+  }
 ];
